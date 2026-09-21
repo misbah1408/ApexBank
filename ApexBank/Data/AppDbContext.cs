6c0d363c7f101a9e61627626ee6bc777db10fbc0
misbah1408 <misbahmohammed00008@gmail.com>
2026-09-21 09:52:40 +0530

    Database initial migration

---
+++ ApexBank/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ApexBank.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApexBank.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<CustomerProfile> CustomerProfiles { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+        public DbSet<RepaymentSchedule> RepaymentSchedules { get; set; }
+        public DbSet<AuditLog> AuditLogs { get; set; }
+    }
+}
--- ApexBank/Program.cs
+++ ApexBank/Program.cs
@@ -1,7 +1,14 @@
+using ApexBank.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// 1. Add DbContext with SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
